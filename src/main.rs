f425fe0f9720a9464e4d27ad094104e0fbc8e3f4
Игорь Батталов <kakamakaka03@yandex.ru>
2024-03-17 17:18:52 +0700

    Добавлена длина диагонали

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn prmtr_area_diag(width: i32, height: i32){
     println!("{}", width * height);
     println!("{}", 2 * (width + height));
+    let diag_is_sqrd: i32 = width.pow(2) + height.pow(2);
+    println!("{}", (diag_is_sqrd as f64).sqrt());
 }
 fn main() {
-    println!("Hello, Igor");
-    let width = 12;
-    let height = 10;
+    let width: i32 = 19;
+    let height: i32 = 10;
     prmtr_area_diag(width, height);
 }
