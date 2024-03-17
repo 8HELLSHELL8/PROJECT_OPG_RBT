692db697858a191ab92aaa9e90c5460871ac62f4
Vlad T <rin705696@gmail.com>
2024-03-17 20:48:20 +0300

    Треугольник

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 fn perimetr_area_isravnobedr(side_1: f64, side_2: f64,side_3: f64){
-    if(side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2 || side_2 + side_3 <= side_1){
+    if side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2 || side_2 + side_3 <= side_1{
         println!("Треугольник не существует")
     }else{
     let perimetr: f64 = side_1 + side_2 + side_3;
     println!("Периметр треугольника равен {}", perimetr);
     let half_perimetr: f64 = (side_1 + side_2 + side_3) / 2.0;
-    let area: f64 = (half_perimetr * (half_perimetr - side_1) * (half_perimetr - side_2) * (half_perimetr - side_3));
+    let area: f64 = half_perimetr * (half_perimetr - side_1) * (half_perimetr - side_2) * (half_perimetr - side_3);
     println!("Площадь треугольника равна {}", area);
 
-    if(side_1 == side_2 || side_1 == side_3 || side_2 == side_3) {
+    if side_1 == side_2 || side_1 == side_3 || side_2 == side_3{
         println!("Треугольник равнобедренный")
-    } else {
+    }else{
         println!("Треугольник неравнобедренный")
     }
 }
