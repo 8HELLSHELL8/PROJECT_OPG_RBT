d6bafe4afe0da4b184719704ccf8b09b7db4fb89
Игорь Батталов <kakamakaka03@yandex.ru>
2024-03-17 16:51:05 +0700

    Добавлены периметр и площадь

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn prmtr_area_diag(width: i32, height: i32){
+    println!("{}", width * height);
+    println!("{}", 2 * (width + height));
+}
 fn main() {
     println!("Hello, Igor");
+    let width = 12;
+    let height = 10;
+    prmtr_area_diag(width, height);
 }
