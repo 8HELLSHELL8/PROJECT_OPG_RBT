ecbd5ae919e2003c92cd6095041a59464f75b33b
8HELLSHELL8 <anton.mgdn@gmail.com>
2024-03-17 13:24:13 +0300

    add solution for trapeze

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn trapeze(little_base:i32, big_base:i32, left_side:i32, right_side:i32, height:i32){
+    //perimetr
+    let perimetr: i32 = little_base + big_base + left_side + right_side;
+
+    //middle line
+    let middleLine: f64 = ((little_base as f64) + (big_base as f64))/2.0;
+
+    //area
+    let area:f64 = middleLine * (height as f64); 
+
+    println!("Данные трапеции:");
+    println!("Периметр = {} ", perimetr);
+    println!("Площадь = {} ", area);
+    println!("Длина средней линии = {} ", middleLine);
+
+}
+
+
 fn main() {
-    println!("Hello, Anton");
+    trapeze(14,26, 10, 10, 8);
 }
