619d23dbafbb539db7b492b7bef5285fba3d697d
8HELLSHELL8 <anton.mgdn@gmail.com>
2024-03-17 16:39:23 +0300

    warn edit

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ fn trapeze(little_base:i32, big_base:i32, left_side:i32, right_side:i32, height:
     let perimetr: i32 = little_base + big_base + left_side + right_side;
 
     //middle line
-    let middleLine: f64 = ((little_base as f64) + (big_base as f64))/2.0;
+    let middle_line: f64 = ((little_base as f64) + (big_base as f64))/2.0;
 
     //area
-    let area:f64 = middleLine * (height as f64); 
+    let area:f64 = middle_line * (height as f64); 
 
     println!("Данные трапеции:");
     println!("Периметр = {} ", perimetr);
     println!("Площадь = {} ", area);
-    println!("Длина средней линии = {} ", middleLine);
+    println!("Длина средней линии = {} ", middle_line);
 
 }
 
