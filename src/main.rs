4d0a8082dc56ae2eb72b7bf83ae879ef1fb50b42
Игорь Батталов <kakamakaka03@yandex.ru>
2024-03-17 17:52:59 +0700

    Добавлены комментарии

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-fn prmtr_area_diag(width: i32, height: i32){
-    println!("{}", width * height);
-    println!("{}", 2 * (width + height));
-    let diag_is_sqrd: i32 = width.pow(2) + height.pow(2);
-    println!("{}", (diag_is_sqrd as f64).sqrt());
+fn prmtr_area_diag(width: i32, height: i32){//Функция выводит на экран значения периметра, площади, длины диагонали прямоугольника
+    println!("Площадь прямоугольника равна {}", width * height);//Вывод площади
+    println!("Периметр прямоугольника равен{}", 2 * (width + height));//Вывод периметра
+    let diag_is_sqrd: i32 = width.pow(2) + height.pow(2);//Переменная diag_is_sqrd - длина диагонали в квадрате
+    println!("Длина диагонали прямоугольника равна{}", (diag_is_sqrd as f64).sqrt());//Вывод диагонали
 }
 fn main() {
-    let width: i32 = 19;
-    let height: i32 = 10;
-    prmtr_area_diag(width, height);
+    let width: i32 = 19;//Ширина прямоугольника
+    let height: i32 = 10;//Высота прямоугольника
+    prmtr_area_diag(width, height);//Вызов функции
 }
